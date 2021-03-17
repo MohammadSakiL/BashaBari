05ddfa3acdaa9db6c3fb69e8d262f7e1154f70cf
MohammadSakil <mahfuzur.shakil@northsouth.edu>
2021-03-18 00:58:47 +0600

    Swipe Refresh option added to Login Page

--- app/src/main/java/com/example/bashabari/_3Login.java
+++ app/src/main/java/com/example/bashabari/_3Login.java
@@ -3,12 +3,14 @@ package com.example.bashabari;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -87,8 +89,27 @@ public class _3Login extends AppCompatActivity {
             }
         });
 
+        try {
+            final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.page_layout_3);
+            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    swipeRefreshLayout.setRefreshing(true);
+                    (new Handler()).postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(_3Login.this, _3Login.class);
+                            startActivity(intent);
+                            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
+                    }, 1000);
+                }
+            });
+        }catch (Exception e)
+        {
 
-
+        }
     }
 
 
