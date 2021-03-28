0683deca2353ad54ba92c2a79769b18d335aa631
NasirSunny50 <nasir.uddin08@northsouth.edu>
2021-03-28 22:14:11 +0600

    User Menu & Notice Design

--- app/src/main/java/com/example/bashabari/_8_Notices.java
+++ app/src/main/java/com/example/bashabari/_8_Notices.java
@@ -2,7 +2,10 @@ package com.example.bashabari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class _8_Notices extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class _8_Notices extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__8__notices);
+    }
+
+    public void hidekeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
