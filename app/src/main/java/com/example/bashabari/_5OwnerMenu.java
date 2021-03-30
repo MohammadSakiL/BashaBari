4b8b47f1bf6f7874591d1409b8ce002cafa88b1e
MohammadSakil <mahfuzur.shakil@northsouth.edu>
2021-03-31 00:26:48 +0600

    Owner menu Notice recycler view added

--- app/src/main/java/com/example/bashabari/_5OwnerMenu.java
+++ app/src/main/java/com/example/bashabari/_5OwnerMenu.java
@@ -8,19 +8,51 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _5OwnerMenu extends AppCompatActivity {
 
     private LinearLayout main_layout,menu_layout;
     private RelativeLayout home_layout;
-    private TextView txtAddTenant,txtSignoutOwner,txtTenantList,txtNotices;
-    private ImageView btnMenu;
+    private TextView txtAddTenant,txtSignoutOwner,txtTenantList,txtNotices,nameTitle,addressTitle;
+    private ImageView btnMenu,see_more_btn;
+
+    private RecyclerView recyclerView;
+    private requestViewAdapter adapter;
+    private List<requestInfo> requestList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__5_owner_menu);
+
+        requestRecyclerView();
+
+        //setContentFromDatabase();
+
+
+        recyclerView = findViewById(R.id.request_recyclerview_5);
+
+        nameTitle = findViewById(R.id.nameTitle_5);
+        addressTitle = findViewById(R.id.addressTitle_5);
+
+        see_more_btn = findViewById(R.id.see_more_btn_5);
 
         main_layout = findViewById(R.id.main_layout_5);
         menu_layout = findViewById(R.id.menu_layout_5);
@@ -40,6 +72,14 @@ public class _5OwnerMenu extends AppCompatActivity {
                 home_layout.setVisibility(View.GONE);
             }
         });
+        see_more_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(_5OwnerMenu.this,_10_More_Requests.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
@@ -90,5 +130,73 @@ public class _5OwnerMenu extends AppCompatActivity {
 
 
 
+    }
+
+    private void requestRecyclerView() {
+        recyclerView = findViewById(R.id.request_recyclerview_5);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        requestList = new ArrayList<>();
+        adapter = new requestViewAdapter(this, requestList);
+        recyclerView.setAdapter(adapter);
+
+        Query query = FirebaseDatabase.getInstance().getReference("Requests Database")
+                .orderByChild("owner").limitToLast(1)
+                .equalTo(readFromFile("111pho111.txt").trim());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                requestList.clear();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        requestInfo req = snapshot.getValue(requestInfo.class);
+                        requestList.add(req);
+                    }
+                    adapter.notifyDataSetChanged();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setContentFromDatabase() {
+        nameTitle.setText(readFromFile("111nam111.txt"));
+        addressTitle.setText(readFromFile("111add111.txt"));
+
+    }
+
+    private String readFromFile(String File_Name){
+        String st = null;
+        FileInputStream fis0 = null;
+        try {
+            fis0 =openFileInput(File_Name);
+            InputStreamReader isr = new InputStreamReader(fis0);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while( (text = br.readLine()) != null ){
+                sb.append(text).append("\n");
+            }
+
+            st = sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(fis0 != null) {
+                try {
+                    fis0.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return st;
     }
 }
