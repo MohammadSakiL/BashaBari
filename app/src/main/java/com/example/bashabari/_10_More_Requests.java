5eead0c6fc5d3e6a424017b1bcafa159b784b902
Tareq69 <mtareq880@gmail.com>
2021-03-31 00:33:25 +0600

    More request and request view adapter added

---
+++ app/src/main/java/com/example/bashabari/_10_More_Requests.java
@@ -0,0 +1,137 @@
+package com.example.bashabari;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _10_More_Requests extends AppCompatActivity {
+
+    private ImageView arrow_btn;
+
+    private RecyclerView recyclerView;
+    private requestViewAdapter adapter;
+    private List<requestInfo> requestList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__10__more__requests);
+
+
+        requestRecyclerView();
+
+        arrow_btn =  findViewById(R.id.back_arrow_btn_10);
+        arrow_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent12 = new Intent(_10_More_Requests.this, _5OwnerMenu.class);
+                startActivity(intent12);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+
+
+        final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.page_layout_10);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                (new Handler()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(_10_More_Requests.this, _10_More_Requests.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
+    }
+
+
+    private void requestRecyclerView() {
+        recyclerView = findViewById(R.id.request_recyclerview_12);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        requestList = new ArrayList<>();
+        adapter = new requestViewAdapter(this, requestList);
+        recyclerView.setAdapter(adapter);
+
+        Query query = FirebaseDatabase.getInstance().getReference("Requests Database")
+                .orderByChild("owner").limitToLast(70)
+                .equalTo(readFromFile("111pho111.txt").trim());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                requestList.clear();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        requestInfo req = snapshot.getValue(requestInfo.class);
+                        requestList.add(req);
+                    }
+                    adapter.notifyDataSetChanged();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    private String readFromFile(String File_Name){
+        String st = null;
+        FileInputStream fis0 = null;
+        try {
+            fis0 =openFileInput(File_Name);
+            InputStreamReader isr = new InputStreamReader(fis0);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while( (text = br.readLine()) != null ){
+                sb.append(text).append("\n");
+            }
+
+            st = sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(fis0 != null) {
+                try {
+                    fis0.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return st;
+
+    }
+}
---
+++ app/src/main/java/com/example/bashabari/requestViewAdapter.java
@@ -0,0 +1,57 @@
+package com.example.bashabari;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class requestViewAdapter extends RecyclerView.Adapter<requestViewAdapter.requestViewHolder> {
+
+    private Context requestContext;
+    private List<requestInfo> requestList;
+
+    public requestViewAdapter(Context requestContext, List<requestInfo> requestList) {
+        this.requestContext = requestContext;
+        this.requestList = requestList;
+    }
+
+    @NonNull
+    @Override
+    public requestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(requestContext).inflate(R.layout.requests_cardview, parent, false);
+        return new requestViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull requestViewHolder holder, int position) {
+        requestInfo req = requestList.get(position);
+        holder.nameShow.setText(req.getName());
+        holder.dateShow.setText(req.getDate());
+        holder.textShow.setText(req.getText());
+        holder.solveShow.setText(req.getSolveStat());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class requestViewHolder extends RecyclerView.ViewHolder {
+        TextView nameShow, dateShow, textShow,solveShow;
+        public requestViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameShow = itemView.findViewById(R.id.name_rv_requests);
+            dateShow = itemView.findViewById(R.id.date_rv_requests);
+            textShow = itemView.findViewById(R.id.text_rv_requests);
+            solveShow = itemView.findViewById(R.id.solve_rv_requests);
+
+        }
+    }
+}
