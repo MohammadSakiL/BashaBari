78714bb0624ea119aa566fe6664cbca520047c8a
NasirSunny50 <nasir.uddin08@northsouth.edu>
2021-04-05 15:31:57 +0600

    More Notices Added

--- app/src/main/java/com/example/bashabari/_11_More_Notices.java
+++ app/src/main/java/com/example/bashabari/_11_More_Notices.java
@@ -1,14 +1,135 @@
 package com.example.bashabari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _11_More_Notices extends AppCompatActivity {
+
+
+    private ImageView back_arrow_btn_11;
+
+    private RecyclerView recyclerView;
+    private noticeViewAdapter adapter;
+    private List<noticeInfo> noticeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__11__more__notices);
+
+        noticeRecyclerView();
+
+        back_arrow_btn_11 = findViewById(R.id.back_arrow_btn_11);
+
+        back_arrow_btn_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent7 = new Intent(_11_More_Notices.this, _6_User_menu.class);
+                startActivity(intent7);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+
+        final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.page_layout_11);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                (new Handler()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(_11_More_Notices.this, _11_More_Notices.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
+    }
+
+    private void noticeRecyclerView() {
+        recyclerView = findViewById(R.id.notice_recyclerview_11);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noticeList = new ArrayList<>();
+        adapter = new noticeViewAdapter(this, noticeList);
+        recyclerView.setAdapter(adapter);
+
+        Query query = FirebaseDatabase.getInstance().getReference("Notice Database")
+                .orderByChild("phone_no").limitToLast(30)
+                .equalTo(readFromFile("111t_ow111.txt").trim());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                noticeList.clear();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        noticeInfo not = snapshot.getValue(noticeInfo.class);
+                        noticeList.add(not);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private String readFromFile(String File_Name){
+
+        String st = null;
+        FileInputStream fis0 = null;
+        try {
+            fis0 =openFileInput(File_Name);
+            InputStreamReader isr = new InputStreamReader(fis0);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while( (text = br.readLine()) != null ){
+                sb.append(text).append("\n");
+            }
+
+            st = sb.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(fis0 != null) {
+                try {
+                    fis0.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return st;
+
     }
 }
